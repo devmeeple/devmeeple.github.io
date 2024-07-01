b8406f10e8f3318e14457f560d8f1b37ebbecef7
미플 <devmeeple@gmail.com>
2024-07-01 23:56:15 +0900

    블로그 설명 수정, '직업, 생각'

--- blog-config.js
+++ blog-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "장태근블로그",
-  description: "명료한 생각이 명료한 글이 된다.",
+  description: "개발자. 명료한 생각이 명료한 글이 된다.",
   author: "장태근",
   siteUrl: "https://devmeeple.github.io/",
   links: {
